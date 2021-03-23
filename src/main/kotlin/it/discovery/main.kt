3bac1cfb48d3501df13be6351f87446d792d58da
Sergiy <sergey.morenets@gmail.com>
2021-03-23 20:18:46 +0200

    Kotlin. class hierarchy and operator when

--- src/main/kotlin/it/discovery/main.kt
+++ src/main/kotlin/it/discovery/main.kt
@@ -1,3 +1,7 @@
+import it.discovery.java8.Discount
+import it.discovery.java8.HalfDiscount
+import it.discovery.java8.NoDiscount
+
 fun main(args: Array<String>) {
     println("Kotlin VS Java")
 }
@@ -11,5 +15,25 @@ data class Product(val id: Int, val price: Double) {
 
     override fun hashCode() = id
 }
+
+class Discount(val discount: Double = 0.0)
+
+object DiscountFactory {
+    fun getDiscount(amount: Int): Discount {
+        return when {
+            amount == 0 -> NoDiscount()
+            amount == 50 -> HalfDiscount()
+            amount < 0 || amount >= 100 -> throw
+                IllegalArgumentException("Invalid discount value: $amount")
+            else -> Discount(amount.toDouble())
+        }
+    }
+}
+
+class HalfDiscount : Discount(50.0)
+
+class NoDiscount : Discount(0.0)
+
+
 
 
