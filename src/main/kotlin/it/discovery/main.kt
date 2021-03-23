25385f1744b8bb771bf29aa1dc2b4408acd43b67
Sergiy <sergey.morenets@gmail.com>
2021-03-23 20:09:47 +0200

    Kotlin. Data classes

--- src/main/kotlin/it/discovery/main.kt
+++ src/main/kotlin/it/discovery/main.kt
@@ -2,6 +2,14 @@ fun main(args: Array<String>) {
     println("Kotlin VS Java")
 }
 
+data class Product(val id: Int, val price: Double) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
 
+        return other is Product && id == other.id
+    }
+
+    override fun hashCode() = id
+}
 
 
